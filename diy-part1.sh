f87b934373f895fb93252252acdba5c20128c8c4
RC1844 <1844766234@qq.com>
2020-11-30 08:27:05 +0800

    auto
    Signed-off-by: RC1844 <1844766234@qq.com>

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,4 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages.git' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small.git' feeds.conf.default
-sed -i '$a src-git theme-rosy https://github.com/rosywrt/luci-theme-rosy.git' feeds.conf.default
+sed -i '$a src-git rosy https://github.com/rosywrt/luci-theme-rosy.git' feeds.conf.default
